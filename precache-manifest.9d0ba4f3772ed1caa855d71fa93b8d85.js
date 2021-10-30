9b8b03a5f2eb839af6db5952647abdfc0a55815b
Tchaka Spierings <tchaka.spierings@gmail.com>
2021-10-30 19:44:14 +0200

    Updates

---
+++ precache-manifest.9d0ba4f3772ed1caa855d71fa93b8d85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d98d1fc26b670f292c146d202e8437f",
+    "url": "/coffee-repository/index.html"
+  },
+  {
+    "revision": "8739e46597f7a8a3f5e4",
+    "url": "/coffee-repository/static/css/main.00d36025.chunk.css"
+  },
+  {
+    "revision": "debc64d6f0f70ba9b7df",
+    "url": "/coffee-repository/static/js/2.f4290c9e.chunk.js"
+  },
+  {
+    "revision": "7827aa7977f8a8cd8bc1f07476294571",
+    "url": "/coffee-repository/static/js/2.f4290c9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8739e46597f7a8a3f5e4",
+    "url": "/coffee-repository/static/js/main.0316e696.chunk.js"
+  },
+  {
+    "revision": "1de0627d2aab1765973c",
+    "url": "/coffee-repository/static/js/runtime-main.94ac4f50.js"
+  }
+]);
